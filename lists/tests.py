a0aeda9015498011141bbd21da48cfb4f18a6e45
rockokechukwu <rockokechukwu@gmail.com>
2018-03-30 23:21:51 +0100

    Add new test can_have_multiple_users

--- lists/tests.py
+++ lists/tests.py
@@ -16,12 +16,6 @@ class HomePageTest(TestCase):
   def test_root_url_resolves_to_home_page_view(self):
     found = resolve('/')
     self.assertEqual(found.func, home_page)
-  
-  def test_home_page_returns_correct_html(self):
-    request = HttpRequest()
-    response = home_page(request)
-    expected_html = render_to_string('home.html')
-    self.assertEqual(response.content.decode(), expected_html)
 
   def test_home_page_can_save_a_post_request(self):
     request = HttpRequest()
