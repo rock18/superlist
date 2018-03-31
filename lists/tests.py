bf9057a50a890952e50eefa8f90c1ebe17172aca
rockokechukwu <rockokechukwu@gmail.com>
2018-03-31 13:28:23 +0100

    Commit changes to templates, views and urls

--- lists/tests.py
+++ lists/tests.py
@@ -17,31 +17,6 @@ class HomePageTest(TestCase):
     found = resolve('/')
     self.assertEqual(found.func, home_page)
 
-  def test_home_page_can_save_a_post_request(self):
-    request = HttpRequest()
-    request.method = "POST"
-    request.POST["item_text"] = "A new list item"
-
-    response = self.client.post('/', data={'item_text': "A new list item"})
-
-    self.assertEqual(Item.objects.count(), 1)
-    new_item = Item.objects.first()
-    self.assertEqual(new_item.text, "A new list item")
-  
-  def test_home_page_can_save_a_POST_and_redirect(self):
-    response = self.client.post('/', data={'item_text':'A new list item'})
-
-    self.assertEqual(response.status_code, 302)
-    self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
-    #self.assertIn("A new list item", response.content.decode())
-    #expected_html = render_to_string('home.html',{'new_item_text':'A new list item'})
-    #self.assertEqual(response.content.decode(), expected_html)
-
-  def test_home_page_only_saves_items_when_neccessary(self):
-    request = HttpRequest()
-    home_page(request)
-    self.assertEqual(Item.objects.count(), 0)
-
 class ItemModelTest(TestCase):
 
   def test_saving_and_retrieving_item(self):
@@ -74,3 +49,18 @@ class ListViewTest(TestCase):
     response = self.client.get('/lists/the-only-list-in-the-world/')
     self.assertContains(response, 'itemey 1')
     self.assertContains(response, 'itemey 2')
+
+class NewListTest(TestCase):
+
+  def test_can_save_a_post_request(self):
+    response = self.client.post('/lists/new', data={'item_text': "A new list item"})
+
+    self.assertEqual(Item.objects.count(), 1)
+    new_item = Item.objects.first()
+    self.assertEqual(new_item.text, "A new list item")
+
+  def test_redirect_after_POST(self):
+    response = self.client.post('/lists/new', data={'item_text':'A new list item'})
+
+    self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+
